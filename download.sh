9a05ef5d1219db1ed67ee2256079670978a607fc
Ridwan Shaikh <48447625+ridwan-shaikh@users.noreply.github.com>
2019-06-21 17:31:42 +0100

    Update download.sh
    Works with bscp

--- download.sh
+++ download.sh
@@ -7,3 +7,8 @@ bs config list
 bs config load EU
 
 #export the resulting API server and access token
+
+
+#the bscp method. Be sure to change the run ID etc. Needs authenticting using the API export etc in the EU config file
+
+Users/ridwanshaikh/bin/bs-cp --exclude *jpg https://api.euc1.sh.basespace.illumina.com/run/2436434 .
