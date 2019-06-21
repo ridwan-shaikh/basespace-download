fc12fcdee13e6d6d497be93756f0700f7e5c9980
Ridwan Shaikh <48447625+ridwan-shaikh@users.noreply.github.com>
2019-06-21 17:17:08 +0100

    download.sh
    instructions to download run from basespace

---
+++ download.sh
@@ -0,0 +1,9 @@
+#script to download files from basespace
+
+#list the possible config files
+bs config list
+
+#load the EU config file to download from the SMP2 repo
+bs config load EU
+
+#export the resulting API server and access token
